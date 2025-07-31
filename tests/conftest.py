f95d625c75354a7c2fc75b47e9e886a41295044d
Gherman Grebenyuk <german.grebenyuk@gmail.com>
2025-07-31 22:51:20 +0300

    installed selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,27 @@
 import pytest
-from selene import browser
 
-@pytest.fixture
-def setup_formpage_chrome():
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.open('https://demoqa.com/automation-practice-form')
-    yield
-    browser.close()
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    browser.quit()
